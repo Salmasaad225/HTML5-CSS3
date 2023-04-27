22606623cf871c85178bd102554c79c14162a5a6
Salma Saad <123007642+Salmasaad225@users.noreply.github.com>
2023-04-27 13:24:09 +0200

    Add files via upload

---
+++ task(3)/index (1).js
@@ -0,0 +1,34 @@
+
+ var img_aA
+
+ function drop_A(a) {
+    img_aA = a
+ }
+
+ function drop(divv) {
+     divv.appendChild(img_aA)
+    
+ }
+
+
+var eye = document.getElementById("path4151-4-06-1-6")
+eye.addEventListener("click", function () {
+    alert("this is eye")
+
+})
+
+var hair = document.getElementById("path4365-0-3-5-3-7")
+hair.addEventListener("click", function () {
+
+    alert("this is hair")
+
+})
+
+
+
+var nose = document.getElementById("path4153-74")
+nose.addEventListener("click", function () {
+
+    alert("this is nose")
+
+})
